f33b57ef58bae345745db95a5ef0b65c0424ece3
Kjell Hanken <69535642+kjell5317@users.noreply.github.com>
2022-07-23 13:26:50 +0200

    appBar & naming conventions

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -25,61 +25,80 @@ class _FeedbackPageState extends State<FeedbackPage> {
   Widget build(BuildContext context) {
     if (FirebaseAuth.instance.currentUser != null) {
       return Scaffold(
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Icon(
-              Icons.feedback,
-              size: 100,
-              color: Colors.orange,
-            ),
+          appBar: AppBar(
+            title: const Text('Feedback'),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () => Navigator.pushNamed(context, '/profile'),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey[400],
+                    child: Text(
+                      user!.displayName?.substring(0, 2).toUpperCase() ?? 'HI',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text('Feedback',
-                  style: Theme.of(context).textTheme.headline3)),
-          Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Form(
-                  key: _formKey,
-                  child: TextFormField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    controller: _controller,
-                    textInputAction: TextInputAction.done,
-                    validator: (String? text) {
-                      if (text == null || text.isEmpty) {
-                        return 'Please enter your feedback';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Your feedback'),
-                  ))),
-          Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      await FirebaseFirestore.instance
-                          .doc('feedback/${user!.uid}')
-                          .set({
-                        DateTime.now().millisecondsSinceEpoch.toString():
-                            _controller.text
-                      }, SetOptions(merge: true));
-                      Navigator.pop(context);
-                    }
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text('Submit', style: TextStyle(fontSize: 18)),
-                  ))),
-        ],
-      ));
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Icon(
+                  Icons.feedback,
+                  size: 100,
+                  color: Colors.orange,
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Feedback',
+                      style: Theme.of(context).textTheme.headline3)),
+              Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        controller: _controller,
+                        textInputAction: TextInputAction.done,
+                        validator: (String? text) {
+                          if (text == null || text.isEmpty) {
+                            return 'Please enter your feedback';
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Your feedback'),
+                      ))),
+              Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await FirebaseFirestore.instance
+                              .doc('feedback/${user!.uid}')
+                              .set({
+                            DateTime.now().millisecondsSinceEpoch.toString():
+                                _controller.text
+                          }, SetOptions(merge: true));
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Submit', style: TextStyle(fontSize: 18)),
+                      ))),
+            ],
+          ));
     } else {
       return const LoginPage();
     }
